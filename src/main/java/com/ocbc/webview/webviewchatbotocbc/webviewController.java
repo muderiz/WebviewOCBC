101cf33eeee9817d2549033889d3eb4ae8231e91
Deka <Deka@DESKTOP-3QJUVTC>
2019-06-27 18:38:57 +0700

    Webview
    New Version

--- src/main/java/com/ocbc/webview/webviewchatbotocbc/webviewController.java
+++ src/main/java/com/ocbc/webview/webviewchatbotocbc/webviewController.java
@@ -6,16 +6,11 @@
 package com.ocbc.webview.webviewchatbotocbc;
 
 import com.ocbc.webview.util.OkHttpUtil;
+import java.math.BigDecimal;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
-import okhttp3.MediaType;
 import okhttp3.Request;
-import okhttp3.RequestBody;
 import okhttp3.Response;
-import org.json.JSONException;
-import java.util.Collections;
-import java.util.Comparator;
-import org.json.JSONArray;
 import org.json.JSONObject;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -28,6 +23,8 @@ import org.springframework.web.bind.annotation.RequestMethod;
  */
 @Controller
 public class webviewController {
+
+   
 
     @RequestMapping(value = {"/pendidikan/{customer_name}/{usia_anak}/{negara}/{dana_tersedia}"}, method = {RequestMethod.GET})
     @GetMapping
@@ -57,30 +54,45 @@ public class webviewController {
     @RequestMapping(value = {"/growth/{Investment_Type}/{Investment_Amount}/{Tenor}"}, method = {RequestMethod.GET})
     @GetMapping
     public String growth(@PathVariable String Investment_Type, @PathVariable String Investment_Amount, @PathVariable String Tenor, Model model) {
-//        try {
-//            OkHttpUtil okHttpUtil = new OkHttpUtil();
-//            okHttpUtil.init(true);
-//            String url = "";
-//            Request request = new Request.Builder().url(url).get().build();
-//            Response response = okHttpUtil.getClient().newCall(request).execute();
-//
-//            JSONObject jsonObject = new JSONObject(response);
-//            int RC = jsonObject.getInt("RC");
-//            String Rate = jsonObject.getString("Rate");
-//            String Result = jsonObject.getString("Result");
-//        } catch (Exception e) {
-//        }
+
+        int ChannelId = 0;
+        int RC = 0;
+        String ExtReffId = "";
+        String RC_Desc = "";
+        String Rate = "";
+        BigDecimal Result = null;
+        try {
+            OkHttpUtil okHttpUtil = new OkHttpUtil();
+            okHttpUtil.init(true);
+            String url = "https://api.myjson.com/bins/6gjuh";
+            Request request = new Request.Builder().url(url).get().build();
+            Response response = okHttpUtil.getClient().newCall(request).execute();
+
+            JSONObject jsonObject = new JSONObject(response.body().string());
+            ChannelId = jsonObject.getInt("Channel_ID");
+            RC = jsonObject.getInt("RC");
+            ExtReffId = jsonObject.getString("Ext_Reff_ID");
+            RC_Desc = jsonObject.getString("RC_Description");
+            Rate = jsonObject.getString("Rate");
+            Result = jsonObject.getBigDecimal("Result");
+
+        } catch (Exception e) {
+        }
 
         model.addAttribute("Investment_Type", Investment_Type);
         model.addAttribute("Investment_Amount", Investment_Amount);
         model.addAttribute("Tenor", Tenor);
+        model.addAttribute("RC", RC);
+        model.addAttribute("Ext_Reff_ID", ExtReffId);
+        model.addAttribute("Rate", Rate);
+        model.addAttribute("Result", Result);
 
         return "growth";
     }
 
     @RequestMapping(value = {"/etc/{customer_name}/{target_dana}/{dana_sekarang}/{jangka_waktu}"}, method = {RequestMethod.GET})
     @GetMapping
-    public String etc(@PathVariable String customer_name, @PathVariable String target_dana, @PathVariable String dana_sekarang, 
+    public String etc(@PathVariable String customer_name, @PathVariable String target_dana, @PathVariable String dana_sekarang,
             @PathVariable String jangka_waktu, Model model) {
 //        try {
 //            OkHttpUtil okHttpUtil = new OkHttpUtil();
@@ -100,12 +112,6 @@ public class webviewController {
         model.addAttribute("target_dana", target_dana);
         model.addAttribute("dana_sekarang", dana_sekarang);
         model.addAttribute("jangka_waktu", jangka_waktu);
-
-        return "etc";
-    }
-    
-    @GetMapping("/etc")
-    public String etc() {
 
         return "etc";
     }
